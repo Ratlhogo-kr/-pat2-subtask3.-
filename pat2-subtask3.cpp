6a26e07930871598a23672cad35de39b3a49cd3e
unknown <202520048kr@gmail.comgit config --global user.name Kea>
2026-07-13 20:32:41 +0200

    Constants of logical and synrax errors

---
+++ pat2-subtask3.cpp
@@ -0,0 +1,7 @@
+include <iostream> // Missing # before include and syntax error
+#include <iomanip>
+using namespace std;
+
+//Constants
+const int NUM_EXPERIMENTS = 4; // Logic error changed into 3 to 4.
+const int NUM_READINGS = 3;
